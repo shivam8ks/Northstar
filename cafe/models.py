bebda955be57b2d53103d7e99df9cc6dc4dc0832
shivam.ks <shivam.ks@incedoinc.com>
2019-07-31 15:07:27 +0530

    changes in cafe_snacks and cafe_cart

--- cafe/models.py
+++ cafe/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 class snacks(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
+    date_time = models.DateField(default=datetime.date.today)
     is_active = models.BooleanField(default=False)
 
 
@@ -17,4 +18,6 @@ class cart(models.Model):
     date_time = models.DateField(default=datetime.date.today)
     total = models.FloatField(null=True)
     payment_status = models.CharField(max_length=50)
+    txn_id = models.CharField(max_length=200, null=True)
+
 
